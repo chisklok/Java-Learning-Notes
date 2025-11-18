0a8832a75f01fbc154d699997c6c3de0555f7257
chisklok <cilok7442@gmail.com>
2025-11-18 23:27:23 +0700

    Create Variables.java

---
+++ basics/Variables.java
@@ -0,0 +1,7 @@
+public class Variables {
+    public static void main(String[] args) {
+        String name = "Hevox";
+        int age = 20;
+        System.out.println(name + " - " + age);
+    }
+}
