7a6fbcd0c186420539f0897afafeeab1dcaf15dd
chisklok <cilok7442@gmail.com>
2025-11-18 23:27:51 +0700

    Create IfElse.java

---
+++ control-flow/IfElse.java
@@ -0,0 +1,7 @@
+public class IfElse {
+    public static void main(String[] args) {
+        int score = 82;
+        if (score >= 80) System.out.println("Nice!");
+        else System.out.println("Keep learning!");
+    }
+}
